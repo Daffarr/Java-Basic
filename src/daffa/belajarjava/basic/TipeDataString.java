4523127406e0060b0026bd611db289a29f354a85
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-02-20 11:54:17 +0700

    Tipe Data String

---
+++ src/daffa/belajarjava/basic/TipeDataString.java
@@ -0,0 +1,16 @@
+package daffa.belajarjava.basic;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+
+        // String
+        String firstName = "Daffa Rayhan";
+        String lastName = "Riadi";
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        // Menggabungkan String
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
