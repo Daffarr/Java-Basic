27727ff22d7b6128e679780a7f64b6e3629cac16
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-02-23 17:05:47 +0700

    Method

---
+++ src/daffa/belajarjava/basic/Method.java
@@ -0,0 +1,18 @@
+package daffa.belajarjava.basic;
+
+public class Method {
+    public static void main(String[] args) {
+
+        sayHelloWorld();
+        sayHelloWorld();
+        sayHelloWorld();
+
+    }
+
+    static void sayHelloWorld() {
+        System.out.println("Hello Word 1");
+        System.out.println("Hello Word 2");
+        System.out.println("Hello Word 3");
+    }
+
+}
